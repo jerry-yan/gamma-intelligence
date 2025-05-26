539f503df865aeaf9a8bf7c1bef8ed7be2c2c7dd
Jerry <jerry-yan@hotmail.com>
2025-05-26 17:46:01 -0400

    Second commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gamma_intelligence.wsgi
---
+++ research_summaries/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ResearchSummariesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'research_summaries'
