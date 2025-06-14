a8a48b9677181d33f61e00e1689ff030399d8005
Jerry <jerry-yan@hotmail.com>
2025-06-14 17:54:37 -0400

    commit 51 - playwright fix what broke pls

--- research_summaries/management/commands/run_pipeline.py
+++ research_summaries/management/commands/run_pipeline.py
@@ -39,7 +39,7 @@ class Command(BaseCommand):
 
                 self.stdout.write('📥 Downloading files...')
                 # call_command('download_files')
-                call_command('download_documents_v2')
+                call_command('download_files_v2')
 
                 self.stdout.write('🧹 Cleaning documents...')
                 call_command('clean_documents')
